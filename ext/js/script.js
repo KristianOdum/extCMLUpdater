217a16fa2e37b0b1640915c125381bfdb4c61641
KristianOdum <kristianodum@gmail.com>
2019-09-13 17:31:26 +0200

    First working version

--- ext/js/script.js
+++ ext/js/script.js
@@ -1,7 +1,33 @@
-function changeUsername() {
-    document.getElementById("Test").value = "Virker!";
-}
+document.getElementById("track").addEventListener("click", p =>{
+    document.getElementById("status").innerHTML = "Tracking...";
+    const xhr = new XMLHttpRequest();
+    const url = "https://crystalmathlabs.com/tracker/update.php?player=ironodum&time=1d"
+    xhr.open("GET", url);
+    xhr.send();
+    xhr.onreadystatechange = p => {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById("status").innerHTML = "Success"
+        }
+    }
+});
 
-function Track(Username) {
 
+
+
+/*
+document.addEventListener('DOMContentLoaded', p => {
+    document.getElementById("NewUsernameInput").addEventListener("keypress", function (e) {
+        var key = e.which || e.keyCode;
+        if (key === 13) {
+            ChangeUsername(document.getElementById("NewUsernameInput").innerText);
+
+            alert(document.getElementById("NewUsernameInput").innerText);
+        }
+    });
+});
+
+function ChangeUsername(NewUsername) {
+    alert(NewUsername);
+    document.getElementById("UsernameDisplay").innerHTML = NewUsername;
 }
+*/
